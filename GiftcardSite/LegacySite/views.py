3420a67d27954153c3e2924c5d65b28c8fd39992
pillurvenkat <32372068+pillurvenkat@users.noreply.github.com>
2020-12-22 13:42:12 -0600

    Update views.py

--- GiftcardSite/LegacySite/views.py
+++ GiftcardSite/LegacySite/views.py
@@ -36,10 +36,10 @@ def register_view(request):
         
         # KG: Uh... I'm not sure this makes sense.
         # Collect data to ensure good password use.
-        if pword not in graphs.keys():
-            graphs[pword] = Counter(f'counter_{pword}', 'The total number of '\
-              + f'times {pword} was used')
-        graphs[pword].inc()
+        #if pword not in graphs.keys():
+        # graphs[pword] = Counter(f'counter_{pword}', 'The total number of '\
+        # + f'times {pword} was used')
+        # graphs[pword].inc()
         pword2 = request.POST.get('pword2', None)
         assert (None not in [uname, pword, pword2])
         if pword != pword2:
